0aa3a64ffa4c5b7334443a0453f6229990145508
Nikhil <86715728+Nikhil23108@users.noreply.github.com>
2026-07-05 23:05:18 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,19 +2,14 @@ using DemoApp.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers(); // Use standard API controllers instead of heavy MVC views
+builder.Services.AddControllers(); 
 builder.Services.AddTransient<EmpModel>();
 
 var app = builder.Build();
 
-// Enable serving static html files from your wwwroot folder
 app.UseDefaultFiles();
 app.UseStaticFiles();
-
 app.UseAuthorization();
-
-// Maps your EmployeeApiController routes (/api/EmployeeApi)
 app.MapControllers();
 
 app.Run();
